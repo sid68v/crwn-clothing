887d12907ffb2c8a0651fab31a42179507116bba
sid68v <sidharthv.cusat@gmail.com>
2019-12-18 21:25:49 +0530

    hompage component division and styling

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../menu-item/menu-item.styles.scss'
+import { sign } from 'crypto';
+
+
+const MenuItem = ({ title, imageUrl, size }) => {
+
+    return (
+        <div className={`menu-item ${size}`} >
+            <div
+                className='background-image'
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            />
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
+
