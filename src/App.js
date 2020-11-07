f61925bdad3815a0bd97c4e45290f1b52d5aab27
sid68v <sidharthv.cusat@gmail.com>
2020-11-08 02:05:26 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
